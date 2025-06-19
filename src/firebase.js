c0e3404a62efbd0614e97f5aca7a683023577589
Soham Deb <sohaam.deb@gmail.com>
2025-06-19 21:59:30 +0530

    Fixed Firebase config storageBucket and built project

--- src/firebase.js
+++ src/firebase.js
@@ -4,7 +4,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDsPTXXsi0QCGCZ2cqelqakcAY2GtaxRIc",
   authDomain: "quickconnect-8d2e7.firebaseapp.com",
   projectId: "quickconnect-8d2e7",
-  storageBucket: "quickconnect-8d2e7.firebasestorage.app",
+  storageBucket: "quickconnect-8d2e7.appspot.com", // 🔍 Corrected here
   messagingSenderId: "126101724049",
   appId: "1:126101724049:web:f96dffee5b023b4df05830",
   measurementId: "G-E0DMK2FTCV"
